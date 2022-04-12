80b4d0bdb0be077f25d2b1e711a758a8be82e1d8
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-04-12 07:29:57 +0200

    Initial Commit

---
+++ src/main/java/zw/co/afrosoft/myblog/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package zw.co.afrosoft.myblog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import zw.co.afrosoft.myblog.domain.Post;
+
+public interface PostRepository extends JpaRepository<Post,Long> {
+}
