4a4b29c6d0e0fd0d7edc35cd9b8352a4deb1ee47
tyfahpundo <tafadzwaallentyfah@gmail.com>
2022-04-21 10:22:17 +0200

    Enabled Swagger Authorization

--- src/main/java/zw/co/afrosoft/myblog/config/SwaggerConfig.java
+++ src/main/java/zw/co/afrosoft/myblog/config/SwaggerConfig.java
@@ -1,23 +1,63 @@
 package zw.co.afrosoft.myblog.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+    private static final String SCHEME_NAME = "bearerScheme";
+    private static final String SCHEME = "Bearer";
+
     @Bean
-    public OpenAPI springShopOpenAPI() {
-        return new OpenAPI()
-                .info(new Info().title("MyBlog APIs")
-                        .description("Spring Blog application")
-                        .version("v0.0.1")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .externalDocs(new ExternalDocumentation()
-                        .description("Developed By Tafadzwa Pundo")
-                        .url("https://springshop.wiki.github.org/docs"));
+    public OpenAPI customOpenAPI() {
+        var openApi = new OpenAPI()
+                .info(getInfo());
+
+        addSecurity(openApi);
+
+        return openApi;
+    }
+
+    private Info getInfo() {
+        return new Info()
+                .title("MyBlog APIs Documentation")
+                .description("The API documentation for MyBlog application.")
+                .version("1.0.0")
+                .license(getLicense());
+    }
+
+    private License getLicense() {
+        return new License()
+                .name("Tafadzwa Allen Pundo")
+                .url("https://www.yours.com/en/");
+    }
+    private void addSecurity(OpenAPI openApi) {
+        var components = createComponents();
+        var securityItem = new SecurityRequirement().addList(SCHEME_NAME);
+
+        openApi
+                .components(components)
+                .addSecurityItem(securityItem);
+    }
+
+    private Components createComponents() {
+        var components = new Components();
+        components.addSecuritySchemes(SCHEME_NAME, createSecurityScheme());
+
+        return components;
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme()
+                .name(SCHEME_NAME)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme(SCHEME);
     }
 }
